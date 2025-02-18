0f23420561fdd278846de5afbeece98c0973d061
lukaQiria <eterigrigolia395@gmail.com>
2025-02-18 20:40:02 +0400

    commit

---
+++ Day 00/Homework/homework.py
@@ -0,0 +1,12 @@
+name = "luka"
+surname = "qiria"
+print(name)
+name = "luka"
+age = "13"
+height = "1.65"
+knows_programming = True
+is_ugly = False
+isUgly = False
+print (name + " " + surname)
+print(name + " " + str(age))
+
