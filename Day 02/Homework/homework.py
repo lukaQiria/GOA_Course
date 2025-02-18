812c1a34b11bbe4a679f323b416e6ed483eefd0d
lukaQiria <eterigrigolia395@gmail.com>
2025-02-18 22:59:38 +0400

    commit

--- Day 02/Homework/homework.py
+++ Day 02/Homework/homework.py
@@ -1,3 +1,12 @@
 from turtle import*
 hobby = " My hobby is soccer. I've been playing soccer for 2 months and I think it's the best sport. I play in central defense and I want to become a professional soccer player. I don't have any other hobbies yet."
 print(hobby)
+
+
+
+
+
+
+
+
+
