ad9f1eb22a4e138a800b1b05eee676866be6d006
lukaQiria <eterigrigolia395@gmail.com>
2025-03-04 21:05:53 +0400

    commit

---
+++ Day 04/Classwork/Homework/homework.py
@@ -0,0 +1,16 @@
+x = 1
+y = 2
+z = 3
+print(x,y,z)
+number = 4
+number2 = 5
+print(number + number2)
+a = 5
+b = 5
+print(a + b)
+age = 13
+#ეს ცვლადის შექმნა და მნიშვენლობის მინიჭება
+age = 14
+#ეს კი ცვლადის მნიშვნელობის შეცვლა
+number = 5
+print("Result: " + str(number))
