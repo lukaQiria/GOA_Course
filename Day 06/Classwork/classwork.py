35cad5f648a69cd47ac3543f34da86ea4b42b36c
lukaQiria <eterigrigolia395@gmail.com>
2025-03-21 20:50:15 +0400

    commit

--- Day 06/Classwork/classwork.py
+++ Day 06/Classwork/classwork.py
@@ -1,8 +1,16 @@
-name = input('hello Luka you are 14 years old')
-print(name)
-num1 = input('please input number 5')
-num2 = input('please input number 4')
-print(int(num1) + int(num2))
-print(int(num1) / int(num2))
-print(int(num1) * int(num2))
-print(int(num1) - int(num2))
+age = input("please input your age: ")
+print(age)
+
+name = input("please input your name: ")
+print("Hello, " + name)
+
+num1 = int(input("input number: "))
+num2 = int(input("input number: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+name = "luka"
+age = "14"
+print("Hello, " + name + " you, are, "+ age + " years old" )
---
+++ Day 06/Homework/Homework.py
@@ -0,0 +1,16 @@
+age = input("please input your age: ")
+print(age)
+
+name = input("please input your name: ")
+print("Hello, " + name)
+
+num1 = int(input("input number: "))
+num2 = int(input("input number: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+name = "luka"
+age = "14"
+print("Hello, " + name + " you, are, "+ age + " years old" )
