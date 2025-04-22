f34df249662f6a8fd418a79e7d64471afa3acf6f
lukaQiria <eterigrigolia395@gmail.com>
2025-04-22 15:59:26 +0400

    commit'

---
+++ Day 10/Classwork/classwork.py
@@ -0,0 +1,11 @@
+print(True and False or False and False or True or True or False)
+#გამოიტანს True-ს უბრალოდ უნდა შევცვალოთ print(False or False or True or True or False)
+#False or False Flase
+#False or True  True
+#True or True  True
+#True or False  True 
+
+True or False and False
+#and სრულდება პირველი
+
+#and გავს გამრავლებას
---
+++ Day 10/Homework/homework.py
@@ -0,0 +1,3 @@
+print(True and not False and (False or True) and True or not False)
+#გამოიტანს True-ს
+#ყველა ნაწილი გამოიტანს True-ს ამიტომაც სულ True-ს გამოიტანს.
