84c2e731bbbfbce92bc91163d6c97a0290038900
lukaQiria <eterigrigolia395@gmail.com>
2025-06-07 17:37:45 +0400

    commit

---
+++ Day 16/Classwork/classwork.py
@@ -0,0 +1,23 @@
+for i in range(1, 31):
+    print(i)
+
+
+
+
+age = 15
+
+for i in range(85):
+    age += 1
+
+print("ასაკი გახდა:", age)
+
+
+for i in range(25, 101, 3):
+    print(i)
+
+
+age = 10
+
+while age < 25:
+    age += 1
+    print("+1 age")
