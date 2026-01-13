a298bb07818a4bde4b58d96b0da51981a6776e16
lukaQiria <eterigrigolia395@gmail.com>
2026-01-13 15:56:34 +0400

    commit

--- test.py/test.py
+++ test.py/test.py
@@ -1,3 +1,17 @@
-numbers = [11, 56, 70, 3, 65]
-bolo = numbers [-1]
-print("bolo ", bolo)
+# weather = input("sheiyvane amindi (mziani,wvimiani,grubliani)")
+# if weather == "mziani":
+#     print("wear a hat")
+# elif weather =="wvimiani":
+#     print("wear boots")
+# elif weather== "grubliani":
+#     print ("wear a jacket")
+# else:
+#     print("wear a shirt")
+
+number = int(input("input your number here:"))
+if number > 0:
+    print("positive")
+elif number < 0:
+    print("negative")
+else:
+    print("zero")
